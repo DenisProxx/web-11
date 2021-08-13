10a2d196cbe34d7268da86e8a5e8c9489bd10901
DenisProxx <84438013+DenisProxx@users.noreply.github.com>
2021-08-13 20:36:12 +0400

    практика джава 2

---
+++ practice-js2/index.js
@@ -0,0 +1,57 @@
+// const num = Number(prompt("Введите число" , 0));
+
+// if (num > 0) {
+//     alert("Число положительное!");
+// }
+// else if(num <0){
+//     alert("Число отрицательное!");
+// }
+// else {
+//     alert("Число равно нулю!");
+// }
+
+// const num = Number(prompt("Введите число" , 0));
+// if (num < 0){
+//     alert("Верно!");
+// }
+// else if (num >120){
+//     alert("Неверно");
+// }
+// else {
+//     alert("Возраст верный");
+// }
+
+// const foo = Number(prompt("Введите число" , 0));
+// if(foo < 0) {
+//     alert(-foo);
+// }
+// else if(foo>0){
+//     alert(foo);
+// }
+
+// const hour = Number(prompt("Введите часы" , 0));
+// const minutes = Number(prompt("Введите минуты" , 0));
+// const secondes = Number(prompt("Введите секунды" , 0));
+//  if(hour > 23 || hour < 0){
+//      alert("Неправильный формат");
+//  }
+//  else if(minutes > 59 || minutes  < 0){
+//     alert("Неправильный формат");
+//  }
+//  else if(secondes > 59 || secondes  < 0){
+//     alert("Неправильный формат");
+//  }
+//  else{
+//      alert("Все верно");
+//  }
+
+const x = Number(prompt("Введите координаты x" , 0));
+const y = Number(prompt("Введите координаты y" , 0));
+ if(x >0 && y >0){
+alert("Первая четверть");
+ }
+
+
+
+
+
