dcd04606b814055f4c64aadc6713beb9d48e88a0
DenisProxx <84438013+DenisProxx@users.noreply.github.com>
2021-08-16 20:46:13 +0400

    циклы while

---
+++ js-3/index.js
@@ -0,0 +1,40 @@
+// const count = prompt("Введите количество" , 0);
+
+// let i = 0;
+
+// while (i < count) {
+//     alert("#");
+//     i++;
+// } 
+
+
+// const count = prompt('Введите число' , 0);
+// let i = count;
+// while (i) {
+//     alert(i);
+//     i--;
+// }
+
+// const a = prompt("Введите число" , 0);
+// let b = prompt("Введите степень" , 0); 
+// let result =1;
+// while  (b > 0){
+// result= result*a; 
+// b--;
+// }
+// alert(result);
+
+// const one = prompt("Введите первое число" , 0);
+// const two = prompt("Введите второе число" , 0);
+
+// let i = one > two ? two : one ;
+//  let result ="";
+// while (i>0) {
+// if (!(one % i) && (two % i)) {
+// result = result ? `${result} , ${i}` : i;
+// }
+// i--;
+// }
+// alert(result);
+
+
